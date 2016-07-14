8ba36bbf774cccfbcbfcefa2d52bb0f746e0e7eb
Roman Gonzalez <roman@unbounce.com>
2016-07-14 18:37:05 +0000

    Add support for faker templates on string operation

--- src/index.js
+++ src/index.js
@@ -1,166 +1,226 @@
-import React from 'react'
-import _ from 'lodash'
-import testcheck, {gen} from 'testcheck'
-import reduce from 'lodash/reduce'
-import extend from 'lodash/extend'
-import isObject from 'lodash/isObject'
+import React from 'react';
+import _ from 'lodash';
+import testcheck, {gen} from 'testcheck';
+import reduce from 'lodash/reduce';
+import extend from 'lodash/extend';
+import isObject from 'lodash/isObject';
+import faker from 'faker';
 
-const undefinedGen = gen.return(undefined)
+const undefinedGen = gen.return(undefined);
 
 // TODO: this is not exhaustive
-const tagNamesGen = gen.oneOf(['div', 'span', 'input', 'img', 'a'])
-const elementGen = gen.map(tagName => React.createElement(tagName), tagNamesGen)
+const tagNamesGen = gen.oneOf(['div', 'span', 'input', 'img', 'a']);
+const elementGen = gen.map(tagName => React.createElement(tagName), tagNamesGen);
 
 // TODO: this is not exaustive
 const nodeGen = gen.oneOf([
   gen.string,
   gen.int,
   elementGen
-])
+]);
 
-const funcGen = gen.return(function noop () {})
+const funcGen = gen.return(function noop() {});
 
 const createSymbol = type => {
-  let result
-  const Symbol = global.Symbol
+  let result;
+  const Symbol = global.Symbol;
 
   if (typeof Symbol === 'function') {
     if (Symbol[type]) {
-      result = Symbol[type]
+      result = Symbol[type];
     } else {
-      result = Symbol(type)
-      Symbol[type] = result
+      result = Symbol(type);
+      Symbol[type] = result;
     }
   } else {
-    result = `@@${type}`
+    result = `@@${type}`;
   }
 
-  return result
+  return result;
+};
+
+const genPropSymbol = createSymbol('gen');
+const metaSymbol = createSymbol('meta');
+
+export function sample(propTypes, opts) {
+  const { maxSize = 10, times = 20 } = opts;
+  return testcheck.sample(genProps(propTypes), {maxSize, times});
 }
 
-const genPropSymbol = createSymbol('gen')
-const metaSymbol = createSymbol('meta')
-
-export function sample (propTypes, opts) {
-  const { maxSize = 10, times = 20 } = opts
-  return testcheck.sample(genProps(propTypes), {maxSize, times})
-}
-
-export function getMeta (obj) {
+export function getMeta(obj) {
   if (obj[metaSymbol]) {
-    return obj[metaSymbol]
+    return obj[metaSymbol];
   }
 
   if (isObject(obj)) {
-    obj[metaSymbol] = _.mapValues(obj, fn => fn[metaSymbol])
-    return obj[metaSymbol]
+    obj[metaSymbol] = _.mapValues(obj, fn => fn[metaSymbol]);
+    return obj[metaSymbol];
   }
 
-  throw new Error('wat?')
+  throw new Error('wat?');
 }
 
-export function genProps (obj) {
+export function genProps(obj) {
   if (obj[genPropSymbol]) {
-    return obj[genPropSymbol]
+    return obj[genPropSymbol];
   }
 
   if (isObject(obj)) {
-    obj[genPropSymbol] = wrapObject(obj)
-    return obj[genPropSymbol]
+    obj[genPropSymbol] = wrapObject(obj);
+    return obj[genPropSymbol];
   }
 
-  throw new Error('wat?')
+  throw new Error('wat?');
 }
 
-function wrapObject (obj) {
-  return reduce(obj, (genAcc, value, key) => {
-    return gen.bind(genAcc, acc =>
-      gen.map(rnd => extend({}, acc, { [key]: rnd }), genProps(value))
-    )
-  }, gen.return({}))
+function wrapObject(obj) {
+  return reduce(
+    obj,
+    (genAcc, value, key) => {
+      return gen.bind(
+        genAcc,
+        acc =>
+          gen.map(rnd => extend({}, acc, { [key]: rnd }), genProps(value))
+      );
+    },
+    gen.return({})
+  );
 }
 
 // TODO dry
-function wrapPrimative (typeFn, primGen, type) {
-  // creates a new fn
-  const fn = typeFn.bind(null)
-  fn[genPropSymbol] = gen.oneOf([primGen, undefinedGen])
-  fn[metaSymbol] = { type, required: false }
+function wrapPrimitive(typeFn, primGen, type) {
 
-  fn.isRequired = typeFn.isRequired.bind(null)
-  fn.isRequired[genPropSymbol] = primGen
-  fn.isRequired[metaSymbol] = { type, required: true }
+  // creates a new fn
+  const fn = typeFn.bind(null);
+  fn[genPropSymbol] = gen.oneOf([primGen, undefinedGen]);
+  fn[metaSymbol] = { type, required: false };
+
+  fn.isRequired = typeFn.isRequired.bind(null);
+  fn.isRequired[genPropSymbol] = primGen;
+  fn.isRequired[metaSymbol] = { type, required: true };
 
   fn.meta = data => {
-    const wrapper = typeFn.bind(null)
-    wrapper[genPropSymbol] = gen.oneOf([primGen, undefinedGen])
-    wrapper[metaSymbol] = { ...data, type, required: false }
+    const wrapper = typeFn.bind(null);
 
-    wrapper.isRequired = typeFn.isRequired.bind(null)
-    wrapper.isRequired[genPropSymbol] = primGen
-    wrapper.isRequired[metaSymbol] = { ...data, type, required: true }
+    if (data.exampleTemplate !== undefined &&
+        type === 'string') {
+      wrapper[genPropSymbol] = gen.oneOf([gen.map(() => faker.fake(data.exampleTemplate),
+                                                  gen.return(null)),
+                                          undefinedGen]);
+    } else {
+      wrapper[genPropSymbol] = gen.oneOf([primGen, undefinedGen]);
+    }
+    wrapper[metaSymbol] = { ...data, type, required: false };
 
-    return wrapper
-  }
+
+    wrapper.isRequired = typeFn.isRequired.bind(null);
+    if (type === 'string' &&
+        data.exampleTemplate !== undefined) {
+      wrapper.isRequired[genPropSymbol] = gen.map(() => faker.fake(data.exampleTemplate),
+                                                  gen.return(null));
+    } else {
+      wrapper.isRequired[genPropSymbol] = primGen;
+    }
+
+    wrapper.isRequired[metaSymbol] = { ...data, type, required: true };
+
+    return wrapper;
+  };
 
   fn.isRequired.meta = data => {
-    const wrapper = typeFn.isRequired.bind(null)
-    wrapper[genPropSymbol] = primGen
-    wrapper[metaSymbol] = { ...data, type, required: true }
+    const wrapper = typeFn.isRequired.bind(null);
 
-    return wrapper
-  }
+    if (type === 'string' &&
+        data.exampleTemplate !== undefined) {
+      wrapper[genPropSymbol] = gen.map(() => faker.fake(data.exampleTemplate),
+                                       gen.return(null));
+    } else {
+      wrapper[genPropSymbol] = primGen;
+    }
 
-  return fn
+    wrapper[metaSymbol] = { ...data, type, required: true };
+
+    return wrapper;
+  };
+
+  return fn;
 }
 
-function wrappedArrayOf (typeFn) {
-  const arrayGen = gen.array(genProps(typeFn))
-  return wrapPrimative(React.PropTypes.arrayOf(typeFn), arrayGen, ['array', typeFn[metaSymbol]])
+function wrappedArrayOf(typeFn) {
+  const arrayGen = gen.array(genProps(typeFn));
+  return wrapPrimitive(
+    React.PropTypes.arrayOf(typeFn),
+    arrayGen,
+    ['array', typeFn[metaSymbol]]
+  );
 }
 
-function wrappedObjectOf (typeFn) {
-  const objGen = gen.object(genProps(typeFn))
-  return wrapPrimative(React.PropTypes.objectOf(typeFn), objGen, ['object', typeFn[metaSymbol]])
+function wrappedObjectOf(typeFn) {
+  const objGen = gen.object(genProps(typeFn));
+  return wrapPrimitive(
+    React.PropTypes.objectOf(typeFn),
+    objGen,
+    ['object', typeFn[metaSymbol]]
+  );
 }
 
-function wrappedShape (typeObj) {
-  const shapeGen = genProps(typeObj)
-  return wrapPrimative(React.PropTypes.shape(typeObj), shapeGen, ['shape', _.mapValues(typeObj, fn => fn[metaSymbol])])
+function wrappedShape(typeObj) {
+  const shapeGen = genProps(typeObj);
+  return wrapPrimitive(
+    React.PropTypes.shape(typeObj),
+    shapeGen,
+    ['shape', _.mapValues(typeObj, fn => fn[metaSymbol])]
+  );
 }
 
-function wrappedOneOfType (typeFns) {
-  const oneOfTypeGen = gen.oneOf(typeFns.map(genProps))
-  return wrapPrimative(React.PropTypes.oneOfType(typeFns), oneOfTypeGen, ['oneOfType', _.map(typeFns, fn => fn[metaSymbol])])
+function wrappedOneOfType(typeFns) {
+  const oneOfTypeGen = gen.oneOf(typeFns.map(genProps));
+  return wrapPrimitive(
+    React.PropTypes.oneOfType(typeFns),
+    oneOfTypeGen,
+    ['oneOfType', _.map(typeFns, fn => fn[metaSymbol])]
+  );
 }
 
-function wrappedOneOf (objs) {
-  const oneOfGen = gen.returnOneOf(objs)
-  return wrapPrimative(React.PropTypes.oneOf(objs), oneOfGen, ['oneOf', objs])
+function wrappedOneOf(objs) {
+  const oneOfGen = gen.returnOneOf(objs);
+  return wrapPrimitive(
+    React.PropTypes.oneOf(objs),
+    oneOfGen,
+    ['oneOf', objs]
+  );
 }
 
 // TODO: this is not entirely correct because an 'instance' can be a class
-function wrappedInstanceOf (Component) {
-  const propsGen = wrapObject(Component.propTypes)
-  const instanceOfGen = gen.map(props => <Component {...props} />, propsGen)
+function wrappedInstanceOf(Component) {
+  const propsGen = wrapObject(Component.propTypes);
+  const instanceOfGen = gen.map(props => <Component {...props} />, propsGen);
 
-  return wrapPrimative(React.PropTypes.instanceOf(Component), instanceOfGen, ['instance', Component])
+  return wrapPrimitive(
+    React.PropTypes.instanceOf(Component),
+    instanceOfGen,
+    ['instance', Component]
+  );
 }
 
 export const PropTypes = {
-  array: wrapPrimative(React.PropTypes.array, gen.array, 'array'),
-  bool: wrapPrimative(React.PropTypes.bool, gen.boolean, 'boolean'),
-  number: wrapPrimative(React.PropTypes.number, gen.int, 'number'),
-  object: wrapPrimative(React.PropTypes.object, gen.object(gen.alphaNumString, gen.any), 'object'),
-  string: wrapPrimative(React.PropTypes.string, gen.alphaNumString, 'string'),
-  any: wrapPrimative(React.PropTypes.any, gen.any, 'anything'),
-  element: wrapPrimative(React.PropTypes.element, elementGen, 'element'),
-  node: wrapPrimative(React.PropTypes.node, nodeGen, 'node'),
-  func: wrapPrimative(React.PropTypes.func, funcGen, 'function'),
+  array: wrapPrimitive(React.PropTypes.array, gen.array, 'array'),
+  bool: wrapPrimitive(React.PropTypes.bool, gen.boolean, 'boolean'),
+  number: wrapPrimitive(React.PropTypes.number, gen.int, 'number'),
+  object: wrapPrimitive(
+    React.PropTypes.object,
+    gen.object(gen.alphaNumString, gen.any),
+    'object'
+  ),
+  string: wrapPrimitive(React.PropTypes.string, gen.alphaNumString, 'string'),
+  any: wrapPrimitive(React.PropTypes.any, gen.any, 'anything'),
+  element: wrapPrimitive(React.PropTypes.element, elementGen, 'element'),
+  node: wrapPrimitive(React.PropTypes.node, nodeGen, 'node'),
+  func: wrapPrimitive(React.PropTypes.func, funcGen, 'function'),
   instanceOf: wrappedInstanceOf,
   oneOf: wrappedOneOf,
   oneOfType: wrappedOneOfType,
   arrayOf: wrappedArrayOf,
   objectOf: wrappedObjectOf,
   shape: wrappedShape
-}
+};
--- test/react-gen-props.test.js
+++ test/react-gen-props.test.js
@@ -1,5 +1,6 @@
-import assert from 'assert'
-import {PropTypes, sample, getMeta} from '../src'
+import assert from 'assert';
+import _ from "lodash";
+import {PropTypes, sample, getMeta} from '../src';
 
 describe('PropTypes', () => {
   it('generates random properties', () => {
@@ -11,29 +12,29 @@ describe('PropTypes', () => {
         color: PropTypes.oneOf(['red', 'blue', 'green']).isRequired,
         sleeveLength: PropTypes.number.isRequired
       }).isRequired
-    }
+    };
 
-    const times = 100
+    const times = 100;
 
-    const result = sample(props, { maxSize: 100, times })
+    const result = sample(props, { maxSize: 100, times });
 
-    assert.equal(result.length, times)
+    assert.equal(result.length, times);
 
     result.forEach(obj => {
-      assert.deepEqual(Object.keys(obj), ['name', 'age', 'isCool', 'shirt'])
-      assert.deepEqual(Object.keys(obj.shirt), ['color', 'sleeveLength'])
-      assert.ok(typeof obj.name === 'string')
-      assert.ok(typeof obj.age === 'number')
-      assert.ok(typeof obj.isCool === 'boolean' || typeof obj.isCool === 'undefined')
-      assert.ok(typeof obj.shirt.color === 'string')
-      assert.ok(typeof obj.shirt.sleeveLength === 'number')
-    })
-  })
+      assert.deepEqual(Object.keys(obj), ['name', 'age', 'isCool', 'shirt']);
+      assert.deepEqual(Object.keys(obj.shirt), ['color', 'sleeveLength']);
+      assert.ok(typeof obj.name === 'string');
+      assert.ok(typeof obj.age === 'number');
+      assert.ok(typeof obj.isCool === 'boolean' || typeof obj.isCool === 'undefined');
+      assert.ok(typeof obj.shirt.color === 'string');
+      assert.ok(typeof obj.shirt.sleeveLength === 'number');
+    });
+  });
 
   it('adds meta properties for easily consumable documentation', () => {
-    const coolMeta = {description: 'Whether someone is cool or something'}
-    const ageMeta = {description: 'The aim of the game'}
-    const shirtMeta = {description: 'The dimensions of your shirt', madeIn: 'China'}
+    const coolMeta = {description: 'Whether someone is cool or something'};
+    const ageMeta = {description: 'The aim of the game'};
+    const shirtMeta = {description: 'The dimensions of your shirt', madeIn: 'China'};
 
     const props = {
       name: PropTypes.string,
@@ -43,9 +44,9 @@ describe('PropTypes', () => {
         color: PropTypes.oneOf(['red', 'blue', 'green']).isRequired,
         sleeveLength: PropTypes.number
       }).isRequired.meta(shirtMeta)
-    }
+    };
 
-    const type = getMeta(props)
+    const type = getMeta(props);
 
     assert.deepEqual(type, {
       name: {
@@ -78,6 +79,36 @@ describe('PropTypes', () => {
         }],
         required: true
       }
-    })
-  })
-})
+    });
+  });
+
+
+
+  it("supports faker templates on string PropTypes", () => {
+
+    const nameMeta = {
+      exampleTemplate: "{{name.firstName}} {{name.lastName}}"
+    };
+
+    const props = {
+      name: PropTypes.string.isRequired.meta(nameMeta),
+      name2: PropTypes.string.meta(nameMeta).isRequired,
+      name3: PropTypes.string.meta(nameMeta)
+    };
+
+    const result = sample(props, { maxSize: 1,
+                                   times: 3 });
+
+    const pairs = _.zip(_.initial(result),
+                        _.tail(result));
+
+    _.each(pairs, ([a, b]) => {
+      assert.ok(a.name !== b.name, "name failed: " + [a.name, b.name]);
+      assert.ok(a.name2 !== b.name2, "name2 failed: " + [a.name2, b.name2]);
+      assert.ok(a.name3 === undefined ||
+                b.name3 === undefined ||
+                a.name3 !== b.name3, "name3 failed: " + [a.name3, b.name3]);
+    });
+  });
+
+});
